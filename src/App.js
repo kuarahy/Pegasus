4851f016cbf297bf3ff676024740faa6f4b15a2d
Lucas <knightfication@gmail.com>
2024-12-31 04:03:46 -0600

    Adding Coming Soon message, and adjusting responsive image a bit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     return (
         <div className="image-container">
             <img src={logo} alt="App Banner" className="responsive-image" />
+            <h1 className="coming-soon">COMING SOON</h1>
         </div>
     );
 }
